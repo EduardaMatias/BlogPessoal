9fc235ec8865054bf4d28d447ad4b95e1a703592
Eduarda Matias <evcm2003@gmail.com>
2022-03-24 15:39:41 -0300

    uptade

---
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,65 @@
+package org.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity /*transforma em tabela*/
+@Table(name="tb_postagens") /*nomeia tabela*/
+public class Postagem {
+	
+	@Id /*informa que é uma chave primária*/
+	@GeneratedValue(strategy = GenerationType.IDENTITY) /*indica o auto increment*/
+	private Long id;
+	
+	@NotNull /*garante que a coluna não vai aceitar valor ´null´*/
+	private String titulo;
+	
+	@NotNull
+	@Size (min = 4, max = 50) /*define valor max e min de caracteres*/
+	private String texto;
+	
+	@UpdateTimestamp /*formata de acordo com a data do computador*/
+	private LocalDateTime data;
+
+	/*getters e setter para pegar e enviar dados*/
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+}

0bc8385b82e1c3adc9b18d065214adff39ca57de
Eduarda Matias <evcm2003@gmail.com>
2022-03-25 15:06:28 -0300

    update

--- src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo); /*buscar todos pelo titulo*/
+public interface PostagemRepository extends JpaRepository<Postagem, Long>
+/*indica que é um repository de postagem*/
+{
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo); 
+	/*buscar todos pelo titulo*/
 }

1b2a00d789f119b801ff48dcd2459294d3f79e51
Eduarda Matias <evcm2003@gmail.com>
2022-04-12 14:59:43 -0300

    update

--- src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,7 +36,17 @@ public class UsuarioController {
 	public ResponseEntity<List<Usuario>> getAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
-
+	
+	@GetMapping ("/{id}")
+	public ResponseEntity<Usuario> getById (@PathVariable Long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping ("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> getByName (@PathVariable String nome){
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));	
+	}
+	
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> cadastraUsuario(@Valid @RequestBody Usuario usuario) {
 		return service.cadastrarUsuario(usuario)
